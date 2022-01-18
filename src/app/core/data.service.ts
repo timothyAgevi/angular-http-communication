5c00622fada965a552bf5e86552dba7243805732
timothyAgevi <timothyagevi@gmail.com>
2022-01-19 02:52:08 +0300

    type observable to getAllBooks method

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { observable } from 'rxjs';
+import { Observable, observable } from 'rxjs';
 import { allBooks, allReaders } from 'app/data';
 import { Reader } from "app/models/reader";
 import { Book } from "app/models/book";
@@ -27,8 +27,8 @@ export class DataService {
     return allReaders.find(reader => reader.readerID === id);
   }
 
-  getAllBooks(): Book[] {
-    return allBooks;
+  getAllBooks():Observable <Book[]> {
+    return ;
   }
 
   getBookById(id: number): Book {
