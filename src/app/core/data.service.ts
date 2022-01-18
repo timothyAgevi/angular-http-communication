0eca560a08c0509be51286be5f1cb4585249dfd3
timothyAgevi <timothyagevi@gmail.com>
2022-01-19 02:29:28 +0300

    import observable to data service

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { observable } from 'rxjs';
 import { allBooks, allReaders } from 'app/data';
 import { Reader } from "app/models/reader";
 import { Book } from "app/models/book";
