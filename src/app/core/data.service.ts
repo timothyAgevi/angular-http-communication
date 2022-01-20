4e8a21656243039e84edc1a7fa3f474753da0b4f
timothyAgevi <timothyagevi@gmail.com>
2022-01-20 14:54:16 +0300

    adjust url

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -32,7 +32,7 @@ export class DataService {
 
   getAllBooks(): Observable<Book[] |BookTrackerError> {
     console.log('Getting all books from the server.');
-    return this.http.get<Book[] >('/api/errors/500')
+    return this.http.get<Book[] >('/api/books')
     .pipe(
       catchError(err=>this.handleHttpError(err))
     );
